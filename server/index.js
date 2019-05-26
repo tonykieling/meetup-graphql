01703de3aabcac301699a28474af298c55936fb0
Tony Kieling <tony.kieling@gmail.com>
2019-05-26 11:30:37 -0700

    setting graphql

--- server/index.js
+++ server/index.js
@@ -1,18 +1,64 @@
-const express = require('express')
-const cors = require('cors')
-const port = 3333
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const graphqlHTTP = require('express-graphql');
+const gql = require('graphql-tag');
+const { buildASTSchema } = require('graphql');
 
-app.use(cors())
+const POSTS = [
+  { author: "John Doe", body: "Hello world" },
+  { author: "Jane Doe", body: "Hi, planet!" },
+];
 
-app.use('/hello', (req, res, next) => {
-  console.log("this is hello")
-  res.send({message: "this is hello"})
-  // next()
-})
+const schema = buildASTSchema(gql`
+  type Query {
+    posts: [Post]
+    post(id: ID!): Post
+  }
+  type Post {
+    id: ID
+    author: String
+    body: String
+  }
+  type Mutation {
+    submitPost(input: PostInput!): Post
+  }
+  input PostInput {
+    id: ID
+    author: String!
+    body: String!
+  }
+`);
 
-app.get ('/', (req, res) => {
-  res.send("this is /")
-})
+const mapPost = (post, id) => post && ({ id, ...post });
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+const root = {
+  posts: () => POSTS.map(mapPost),
+  post: ({ id }) => mapPost(POSTS[id], id),
+  submitPost: ({ input: { id, author, body } }) => {
+    const post = { author, body };
+    let index = POSTS.length;
+  
+    if (id != null && id >= 0 && id < POSTS.length) {
+      if (POSTS[id].author !== author) return null;
+  
+      POSTS.splice(id, 1, post);
+      index = id;
+    } else {
+      POSTS.push(post);
+    }
+  
+    return mapPost(post, index);
+  },
+};
+
+const app = express();
+app.use(cors());
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+const port = process.env.PORT || 4000
+app.listen(port);
+console.log(`Running a GraphQL API server at localhost:${port}/graphql`);
