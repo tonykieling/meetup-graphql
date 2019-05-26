924dd58f312594004ac55f9cf786242c7631807b
Tony Kieling <tony.kieling@gmail.com>
2019-05-26 10:54:00 -0700

    starting

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const port = 3333
+const app = express()
+
+app.use(cors())
+
+app.use('/hello', (req, res, next) => {
+  console.log("this is hello")
+  res.send({message: "this is hello"})
+  // next()
+})
+
+app.get ('/', (req, res) => {
+  res.send("this is /")
+})
+app.listen(port, () => console.log(`Server running on port ${port}`))
